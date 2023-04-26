382ee481043820baa0619898918ef97d4f6ebc83
Ivan Pachev <mr.pachev@abv.bg>
2023-04-26 15:24:29 +0300

    RandomSentences

---
+++ RandomSentences.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RandomSentences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = {"Peter", "Michel", "Jane", "Steve"};
+        String[] places = {"Sofia", "Plovdiv", "Varna", "Burgas"};
+        String[] verbs = {"eats", "holds", "sees", "plays with", "brings"};
+        String[] nouns = {"stones", "cake", "apple", "laptop", "bikes"};
+        String[] adverbs = {"slowly", "diligently", "warmly", "sadly", "rapidly"};
+        String[] details = {"near the river", "at home", "in the park", "sadly", "rapidly"};
+
+        
+
+    }
+}
