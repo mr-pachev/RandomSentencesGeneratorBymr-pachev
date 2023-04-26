60b776dae288db9bc8f53d0aa0b06bea68acdad9
Ivan Pachev <mr.pachev@abv.bg>
2023-04-26 15:32:18 +0300

    Update RandomSentences.java

--- RandomSentences.java
+++ RandomSentences.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RandomSentences {
@@ -11,7 +12,21 @@ public class RandomSentences {
         String[] adverbs = {"slowly", "diligently", "warmly", "sadly", "rapidly"};
         String[] details = {"near the river", "at home", "in the park", "sadly", "rapidly"};
 
-        
+        while (true) {
+            String randomName = getRandomWords(names);
+            String randomPlace = getRandomWords(names);
+            String randomVerb = getRandomWords(names);
+            String randomNoun = getRandomWords(names);
+            String randomAdverb = getRandomWords(names);
+            String randomDetail = getRandomWords(names);
+        }
 
+    }
+
+    public static String getRandomWords(String[] words) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String word = words[randomIndex];
+        return word;
     }
 }
